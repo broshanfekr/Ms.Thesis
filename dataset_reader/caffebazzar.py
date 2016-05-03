75e4bb49c0f05f4ecc88da4bcde88fea8fb18c51
Behnam <bero.1370@yahoo.com>
2016-05-03 16:44:56 +0430

    Delete caffebazzar.py

--- dataset_reader/caffebazzar.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'BeRo'
-
-s = 5
-file_path = "C:Users\BeRo\Desktop\CafeBazaar_comments\comments"
-data_file = open(file_path, 'r')
-data = data_file.readlines()
-Num_Of_Samples = len(data)
