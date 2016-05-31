337876045fd15641b514402ccd1c33d218279064
Behnam Roshanfekr <bero.1370@yahoo.com>
2016-05-31 14:14:48 +0430

    Update text_cnn.py

--- IMDB_CNN/text_cnn.py
+++ IMDB_CNN/text_cnn.py
@@ -8,11 +8,11 @@ class TextCNN(object):
     Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
     """
     def __init__(
-        self, sequence_length, num_classes, vocab_size,
-        embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
+        self, sequence_length, num_classes,
+        embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0, l2_constraint = 1):
 
         # Placeholders for input, output and dropout
-        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
+        self.input_x = tf.placeholder(tf.float32, [None, sequence_length, embedding_size], name="input_x")
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
@@ -21,9 +21,11 @@ class TextCNN(object):
 
         # Embedding layer
         with tf.device('/cpu:0'), tf.name_scope("embedding"):
-            W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name="W")
-            self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
-            self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+            #W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name="W")
+            #self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
+            #self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+            self.embedded_chars_expanded = tf.expand_dims(self.input_x, -1)
+
 
         # Create a convolution + maxpool layer for each filter size
         pooled_outputs = []
@@ -65,9 +67,17 @@ class TextCNN(object):
                 "W",
                 shape=[num_filters_total, num_classes],
                 initializer=tf.contrib.layers.xavier_initializer())
+            p = tf.Variable(tf.constant(0.5, shape=[num_filters_total, num_classes]), name = "p")
             b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
             l2_loss += tf.nn.l2_loss(W)
             l2_loss += tf.nn.l2_loss(b)
+            normal_val = tf.nn.l2_normalize(x=W, dim=0)
+            self.normalizing = tf.Variable.assign(self=W, value=normal_val*l2_constraint)
+            self.last_layer_weight = W
+
+            final_val = tf.mul(x=W, y=p, name="final_val")
+            self.assine_final_value = tf.Variable.assign(self=W, value=final_val)
+
             self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
             self.predictions = tf.argmax(self.scores, 1, name="predictions")
 
@@ -79,4 +89,5 @@ class TextCNN(object):
         # Accuracy
         with tf.name_scope("accuracy"):
             correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
+            self.correct_predictions = correct_predictions
             self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
