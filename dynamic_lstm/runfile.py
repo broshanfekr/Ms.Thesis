4262f924f572e4e6da0c2fa46a910226b92ce3a8
Behnam Roshanfekr <bero.1370@yahoo.com>
2016-10-18 23:35:00 +0330

    Update runfile.py

--- dynamic_lstm/runfile.py
+++ dynamic_lstm/runfile.py
@@ -1,2 +1,2 @@
 import os
-os.system("python3 train_LSTM.py -input_dataset_path input_data/total.tar -word2vec_model_path word2vec_model/IMDB_skipgram_model -output_path bidirectional_result.txt -num_epochs 5 -batch_size 100")
+os.system("python3 train_LSTM.py -input_dataset_path input_data/total.tar -word2vec_model_path word2vec_model/IMDB_skipgram_model -output_path bidirectional_result.txt -num_epochs 30 -batch_size 100")
