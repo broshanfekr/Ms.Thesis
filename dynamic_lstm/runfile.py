db230cc56d6bcaf58112d93117e37aa22e61b8b5
Behnam Roshanfekr <bero.1370@yahoo.com>
2017-04-25 00:04:32 +0430

    Delete runfile.py

--- dynamic_lstm/runfile.py
+++
@@ -1,2 +0,0 @@
-import os
-os.system("python3 train_LSTM.py -input_dataset_path input_data/total.tar -word2vec_model_path word2vec_model/IMDB_skipgram_model -output_path bidirectional_result.txt -num_epochs 30 -batch_size 100")
