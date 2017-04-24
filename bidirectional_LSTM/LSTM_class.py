6e48d2decf14c856581d8182b87db69b37929c81
Behnam Roshanfekr <bero.1370@yahoo.com>
2017-04-25 02:20:25 +0430

    Add files via upload

--- bidirectional_LSTM/LSTM_class.py
+++ bidirectional_LSTM/LSTM_class.py
@@ -1,27 +1,15 @@
 import tensorflow as tf
 import numpy as np
 
-def dynamic_bidirectional_LSTM(input_x, embedding_size, seq_max_len, seq_len_list, n_hidden, weights, biases,dropout_keep_prob):
-    # Prepare data shape to match `rnn` function requirements
-    # Current data input shape: (batch_size, sequence_length, embedding_size)
-    # Required shape: 'sequence_length' tensors list of shape (embedding_size, n_input)
-    # Permuting batch_size and sequence_length
-    #with tf.device('/cpu:0'), tf.name_scope("embedding"):
-        #W_embedding = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name='W_embedding')
-        #self.embedded_chars = tf.nn.embedding_lookup(W_embedding, self.input_x)
-        #embedded_chars = tf.transpose(input_x, [1, 0, 2], name='transpose_input')
-    #embedded_chars = tf.reshape(input_x, [-1, embedding_size], name='reshape_imput')
+def dynamic_bidirectional_LSTM(input_x, seq_len_list, n_hidden, weights, biases,dropout_keep_prob):
 
     with tf.name_scope("mapping_to_hidden"):
 
-        #hidden_matrix = tf.add(tf.matmul(embedded_chars, weights['W_hidden']), biases['B_hidden'])
-        #hidden_matrix = tf.reshape(hidden_matrix, [-1, seq_max_len, n_hidden])
-        #hidden_matrix = tf.split(0, seq_max_len, hidden_matrix, name="hidden_matrix")
         hidden_matrix = input_x
 
         # Define a lstm cell with tensorflow
-        lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden, state_is_tuple=True)
-        lstm_cell = tf.nn.rnn_cell.DropoutWrapper(cell=lstm_cell, output_keep_prob=dropout_keep_prob)
+        lstm_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden, state_is_tuple=True)
+        lstm_cell = tf.contrib.rnn.DropoutWrapper(cell=lstm_cell, output_keep_prob=dropout_keep_prob)
 
         outputs, states = tf.nn.bidirectional_dynamic_rnn(cell_fw=lstm_cell,
                                                           cell_bw=lstm_cell,
@@ -32,25 +20,13 @@ def dynamic_bidirectional_LSTM(input_x, embedding_size, seq_max_len, seq_len_lis
         output_fw, output_bw = outputs
         states_fw, states_bw = states
 
-        outputs = tf.concat(2 ,[output_fw, output_bw])
+        outputs = tf.concat([output_fw, output_bw], 2)
 
         seq_len_list = tf.reshape(seq_len_list, [-1, 1])
         seq_len_tmp = tf.cast(seq_len_list, tf.float32)
 
         sum_outputs = tf.reduce_sum(outputs, reduction_indices=1)
         normal_output = tf.div(sum_outputs, seq_len_tmp)
-        #normal_output = tf.nn.l2_normalize(sum_outputs, dim=1)
-
-        # 'outputs' is a list of output at every timestep, we pack them in a Tensor
-        # and change back dimension to [batch_size, sequence_length, embedding_size]
-        #batch_size = tf.shape(outputs)[0]
-        # Start indices for each sample
-        #index = tf.range(0, batch_size) * seq_max_len + (tf.to_int32(seq_len_list) - 1)
-        # Indexing
-        #normal_output = tf.gather(tf.reshape(outputs, [-1, 2*n_hidden]), index)
-        #mean_outputs = tf.reduce_mean(outputs, reduction_indices=1)
-        #sum_outputs = tf.reduce_sum(outputs, reduction_indices=1)
-        #normal_output = tf.nn.l2_normalize(mean_outputs, dim=0)
 
 
 
--- bidirectional_LSTM/data_helpers.py
+++ bidirectional_LSTM/data_helpers.py
@@ -1,8 +1,6 @@
 import numpy as np
 import itertools
 from collections import Counter
-import tarfile
-from gensim.models import Doc2Vec
 from gensim.models import Word2Vec
 import copy
 from nltk.tokenize import wordpunct_tokenize
@@ -10,17 +8,13 @@ import random
 
 
 def Load_Model(name='./myIMDB_model.d2v'):
-    return Doc2Vec.load(name)
+    return Word2Vec.load(name)
 
-def clean_str(review_docs, max_seq_len_cutoff, is_decode = True):
+def clean_str(review_docs, max_seq_len_cutoff = 50):
 
     output_docs = []
 
     for string in review_docs:
-        if(is_decode==True):
-            string = string.decode("utf-8")
-        else:
-            pass
         words = wordpunct_tokenize(string)
 
         if(len(words) > max_seq_len_cutoff):
@@ -32,60 +26,6 @@ def clean_str(review_docs, max_seq_len_cutoff, is_decode = True):
         output_docs.append(words)
     return output_docs
 
-def Load_IMDB_Data_and_Label(dataset_file_name= "total.tar", max_seq_len_cutoff=1350, is_remove_stopwords = False):
-
-    tar = tarfile.open(dataset_file_name)
-    for member in tar.getmembers():
-        file_name = member.name.split("-")
-        isTrain = file_name[0]
-        data_label = file_name[-1].split(".")[0]
-        f = tar.extractfile(member)
-        content = f.readlines()
-        f.close()
-        if(isTrain == "train"):
-            if(data_label == "pos"):
-                positive_examples = content
-                print('posetive data parsed from training set')
-            elif(data_label == "neg"):
-                negative_examples = content
-                print("negative data parsed from training set")
-            else:
-                pass
-        elif(isTrain == "test"):
-            if(data_label == "pos"):
-                test_positive_examples = content
-                print("test positive examples loaded")
-            elif(data_label == "neg"):
-                test_negative_examples = content
-                print("negative sentences parsed from testing set")
-    tar.close()
-
-    positive_examples = [s.strip() for s in positive_examples]
-    negative_examples = [s.strip() for s in negative_examples]
-    test_positive_examples = [s.strip() for s in test_positive_examples]
-    test_negative_examples = [s.strip() for s in test_negative_examples]
-
-    # Split by words
-    x_text = positive_examples + negative_examples
-    test_x_text = test_positive_examples + test_negative_examples
-
-    x_text = clean_str(x_text, max_seq_len_cutoff)#[clean_str(sent) for sent in x_text]
-    #x_text = [s.split(" ") for s in x_text]
-
-    test_x_text = clean_str(test_x_text, max_seq_len_cutoff)#[clean_str(sent) for sent in test_x_text]
-    #test_x_text = [s.split(" ") for s in test_x_text]
-
-    # Generate labels
-    positive_labels = [[0, 1] for _ in positive_examples]
-    negative_labels = [[1, 0] for _ in negative_examples]
-
-    test_positive_labels = [[0, 1] for _ in test_positive_examples]
-    test_negative_labels = [[1, 0] for _ in test_negative_examples]
-
-    y = np.concatenate([positive_labels, negative_labels], 0)
-    test_y = np.concatenate([test_positive_labels, test_negative_labels], 0)
-    return [x_text, y, test_x_text, test_y]
-
 def build_vocab(sentences):
     """
     Builds a vocabulary mapping from word to index based on the sentences.
@@ -95,18 +35,22 @@ def build_vocab(sentences):
     word_counts = Counter(itertools.chain(*sentences))
     # Mapping from index to word
     vocabulary_inv = [x[0] for x in word_counts.most_common()]
+    vocabulary_inv.append('<pad>')
     vocabulary_inv = list(sorted(vocabulary_inv))
     # Mapping from word to index
     vocabulary = {x: i for i, x in enumerate(vocabulary_inv)}
     return [vocabulary, vocabulary_inv]
 
-def build_input_data(sentences, labels, vocabulary):
+def build_input_data(sentences, vocabulary):
     """
     Maps sentencs and labels to vectors based on a vocabulary.
     """
-    x = np.array([[vocabulary[word] for word in sentence] for sentence in sentences])
-    y = np.array(labels)
-    return [x, y]
+    index_list = []
+    for word in sentences:
+        tmp = vocabulary[word]
+        index_list.append(tmp)
+    x = np.array(index_list)
+    return x
 
 def build_input_data_from_word2vec(sentence, word2vec_vocab, word2vec_vec):
     """
@@ -115,85 +59,60 @@ def build_input_data_from_word2vec(sentence, word2vec_vocab, word2vec_vec):
     X_data = []
     for word in sentence:
         try:
-            word2vec_index = word2vec_vocab[word]
+            word2vec_index = word2vec_vocab[word].index
             word_vector = word2vec_vec[word2vec_index]
         except:
-            word2vec_index = word2vec_vocab['<un_known>']
+            word2vec_index = word2vec_vocab['<un_known>'].index
             word_vector = word2vec_vec[word2vec_index]
-            #word_vector = np.random.uniform(low=-0.00001, high=0.00001, size=len(word2vec_vec[0]))
+            #word_vector = np.random.uniform(low=-0.25, high=0.25, size=word2vec_vec.shape[1])
         X_data.append(word_vector)
     X_data = np.asarray(X_data)
     return X_data
 
-def load_data(dataset_path, word2vec_model_path, n_class=2, max_seq_len_cutoff=1350):
+def load_data(dataset_path, word2vec_model_path, n_class=2, max_seq_len_cutoff=50):
     """
     Loads and preprocessed data from dataset file.
     """
-    if(dataset_path.split("/")[-1] == "total.tar"):
-        sentences, labels, test_sentences, test_labels = Load_IMDB_Data_and_Label(dataset_path, max_seq_len_cutoff)
-        x_text = sentences + test_sentences
-        y = np.concatenate([labels, test_labels], 0)
-    else:
-        dataset_file = open(dataset_path, "r")
-        dataset_content = dataset_file.readlines()
 
-        x_text = []
-        y = []
-        for element in dataset_content:
-            element = element.lower()
-            element = element.split("\t")
-            label = int(element[0])
-            text = element[1].strip()
-            if (len(text) == 0):
-                continue
-            x_text.append(text)
-            tmp_lable = np.zeros(n_class)
-            if(n_class == 2):
-                tmp_lable[label] = 1
-            else:
-                tmp_lable[label - 1] = 1
-            y.append(tmp_lable)
+    dataset_file = open(dataset_path, "r", encoding='utf-8')
+    dataset_content = dataset_file.readlines()
 
-        x_text = clean_str(x_text, max_seq_len_cutoff, is_decode=False)
+    x_text = []
+    y = []
+    for element in dataset_content:
+        element = element.lower()
+        element = element.split("\t")
+        label = int(element[0])
+        text = element[1].strip()
+        if (len(text) == 0):
+            continue
+        x_text.append(text)
+        tmp_lable = np.zeros(n_class)
+        if(n_class == 2):
+            tmp_lable[label] = 1
+        else:
+            tmp_lable[label - 1] = 1
+        y.append(tmp_lable)
 
+
+    x_text = clean_str(x_text, max_seq_len_cutoff)
 
     sequence_length = max(len(x) for x in x_text)
 
     vocabulary, vocabulary_inv = build_vocab(x_text)
     y = np.asarray(y)
 
-
-    word2vec_vocab = {}
-    word2vec_vec = []
-    vector_file = open(word2vec_model_path, 'r')
-    vector_content = vector_file.readlines()
-    for index, word in enumerate(vector_content):
-        if(index == 0):
-            continue
-        word = word.strip()
-        word = word.split()
-
-        line_word = word[0]
-        vec = word[1:]
-        float_vec = [float(v) for v in vec]
-        word2vec_vec.append(float_vec)
-
-        word2vec_vocab[line_word] = index-1
-
-
-
-    word_vector = np.random.uniform(low=-0.25, high=0.25, size=(1,len(word2vec_vec[0])))
-    float_vec = [v for v in word_vector]
-    word2vec_vec.append(float_vec)
-    tmpindex = len(word2vec_vec)-1
-    word2vec_vocab['<un_known>'] = tmpindex
-
-    '''
     word2vec_Model = Load_Model(word2vec_model_path)
     word2vec_vocab = word2vec_Model.vocab
     word2vec_vec = word2vec_Model.syn0
-    '''
 
+    print("word2vec len is: ", len(word2vec_vec))
+    tmp = word2vec_vocab['real']
+    tmp1 = copy.deepcopy(tmp)
+    word_vector = np.random.uniform(low=-0.25, high=0.25, size=(1,word2vec_vec.shape[1]))
+    word2vec_vec = np.append(word2vec_vec, word_vector, axis=0)
+    tmp1.index = len(word2vec_vec)-1
+    word2vec_vocab['<un_known>'] = tmp1
 
     return [x_text, y, sequence_length, vocabulary, vocabulary_inv, word2vec_vocab, word2vec_vec]
 
